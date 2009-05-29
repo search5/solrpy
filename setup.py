7b59f5b150605cd557b90e1a2c830831e0ad2685
benliles <benliles@localhost>
2009-05-29 19:04:41 +0000

    Fix for the setup.py since there is more than just solr.py in the package now

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 import ez_setup
 ez_setup.use_setuptools()
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'solrpy',
     version = '0.6',
     url = 'http://code.google.com/p/solrpy',
     license = 'http://opensource.org/licenses/apache2.0.php',
-    py_modules = ['solr'],
+    packages=find_packages(exclude=['ez_setup']),
     install_requires = [],
     description = 'client for the solr search service',
     tests_require = ["nose>=0.10.1"],
